bd704b57967f6e6e0636d01cd29d8f06418704e7
Alexey Novikov <to.alexey.novikov@gmail.com>
2024-10-02 23:40:57 +0300

    Adding DonorSearchOCR project

---
+++ DonorSearcOCR/app/src/app.py
@@ -0,0 +1,74 @@
+import uvicorn
+import argparse
+import logging
+from model import prepare_davit, evaluate_image
+from fastapi import FastAPI, UploadFile, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/tmp", StaticFiles(directory="tmp"), name='images')
+templates = Jinja2Templates(directory="templates")
+
+app_logger = logging.getLogger(__name__)
+app_logger.setLevel(logging.INFO)
+app_handler = logging.StreamHandler()
+app_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+app_handler.setFormatter(app_formatter)
+app_logger.addHandler(app_handler)
+
+INP_SIZE = 512
+DEVICE = "cpu"
+MODEL_NAME = 'davit_huge_fl'
+
+
+@app.get("/health")
+def orient():
+    return {"status": 'OK'}
+
+
+@app.get("/")
+def main(request: Request):
+    return templates.TemplateResponse("start_form.html",
+                                      {"request": request})
+
+
+@app.post("/predict-recognize")
+def process_request(file: UploadFile, request: Request):
+    model = prepare_davit(MODEL_NAME, '../weights/davit_weights.pt', 4, DEVICE)
+
+    """save file to the local folder and send the image to the process function"""
+    class_name = {0:'0', 1:'90', 2:'180', 3:'270'}
+    img_tmp_path = "tmp/" + file.filename
+    app_logger.info(f'loading image to {img_tmp_path}')
+    with open(img_tmp_path, "wb") as fid:
+        fid.write(file.file.read())
+
+    rotated_img, ind, status = evaluate_image(img_tmp_path, model)
+    if status == 'OK':
+        app_logger.info(f'recognition: input image orientation was {class_name[ind]}')
+        message = f"Input picture was recognized to be oriented at {class_name[ind]} degrees"
+        rot_img_tmp_path = "tmp/" + "rotated_" + file.filename
+        rotated_img.save(rot_img_tmp_path)
+        return templates.TemplateResponse("recognition_form.html",
+                                          {"request": request,
+                                           "class_name": class_name[ind],
+                                           "path": img_tmp_path,
+                                           "path_rotated": rot_img_tmp_path,
+                                           "message": message})
+    else:
+        app_logger.warning(f'some problems {status}')
+        return templates.TemplateResponse("error_form.html",
+                                          {"request": request,
+                                           "result": status,
+                                           "name": file.filename})
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", default=8000, type=int, dest="port")
+    parser.add_argument("--host", default="0.0.0.0", type=str, dest="host")
+    args = vars(parser.parse_args())
+
+    uvicorn.run(app, **args)
---
+++ DonorSearcOCR/app/src/model.py
@@ -0,0 +1,76 @@
+import timm
+import torch
+import logging
+import albumentations as A
+import numpy as np
+import matplotlib.pyplot as plt
+from albumentations.pytorch.transforms import ToTensorV2
+from pathlib import Path
+from PIL import Image
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+
+m_logger = logging.getLogger(__name__)
+m_logger.setLevel(logging.DEBUG)
+handler_m = logging.StreamHandler()
+formatter_m = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+handler_m.setFormatter(formatter_m)
+m_logger.addHandler(handler_m)
+
+IMG_SIZE = 512
+DEVICE = "cpu"
+MEAN = [1.3638, 1.5033, 1.6996]
+STD = [1.0195, 1.0658, 1.0899]
+
+def prepare_davit(model_name: str, params_path: str, num_classes: int, device: str):
+    """Initiates DaViT Visual Transformer as a model
+    Loads model weights for the model"""
+    model = timm.create_model(model_name, pretrained=False, num_classes=num_classes)
+    model = model.to(device)
+    model.load_state_dict(torch.load(params_path, map_location=torch.device(device))['model_state_dict'])
+    m_logger.info(f'model loaded')
+    return model
+
+def evaluate_image(image_path, model, mean=MEAN, std=STD):
+    def transform_sample(image):
+        w, h = image.shape[0], image.shape[1]
+        max_wh = np.max([w, h])
+        aug = A.Compose([
+            A.PadIfNeeded(min_height=max_wh,
+                          min_width=max_wh,
+                          border_mode=0,
+                          value=(255, 255, 255)),
+            A.Resize(IMG_SIZE, IMG_SIZE),
+            A.Normalize(
+                mean=mean,
+                std=std),
+            ToTensorV2()
+        ])
+        return aug(image=image)['image']
+    try:
+        image = Image.open(image_path)
+        img = np.array(image.convert('RGB'))
+    except PIL.UnidentifiedImageError:
+        m_logger.error(f'something wrong with image')
+        status = 'Fail'
+        return status, image_path
+    angles = {0: 0, 1: 90, 2: 180, 3: 270}
+    transformed_img = transform_sample(img).unsqueeze(dim=0)
+    transformed_img = transformed_img.to(DEVICE)
+    with torch.no_grad():
+        model.eval()
+        try:
+            output = model(transformed_img)
+            index = output.data.cpu().numpy().argmax()
+            rotated_img = image.rotate(angles[index], expand=True)
+            status = 'OK'
+            m_logger.info(f'recognition completed')
+        except:
+            status = 'Fail'
+            m_logger.info(f'recognition failed')
+            return status
+    return rotated_img, index, status
+
+
+
